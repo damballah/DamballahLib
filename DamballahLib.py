33e0fd606a32a08d185e426549ed4e9fbde7e242
damballah <pougaud@gmail.com>
2018-03-11 09:19:55 +0100

    Update DamballahLib.py

--- DamballahLib.py
+++ DamballahLib.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-r="toto"
-print (r)
+
+def droite_de (chaine,nbcar) :
+	droite_de = chaine[-nbcar:]
+	return droite_de
+
+def gauche_de (chaine,nbcar) :
+	gauche_de = chaine[:nbcar]
+	return gauche_de
+
+def milieu_de (chaine,debut,longueur):
+	milieu_de = chaine[debut:debut+longueur]
+	return milieu_de
